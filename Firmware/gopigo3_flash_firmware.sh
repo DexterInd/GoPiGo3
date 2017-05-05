c6c798f4fb20f2f72482e88a9eee8a6eaa6586f2
Shoban94 <shobannarayan@gmail.com>
2017-05-05 20:15:55 +0530

    Replacing with a new file

--- Firmware/gopigo3_flash_firmware.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-
-#rm "/home/pi/GoPiGo3_Firmware.bin"
-
-#echo "Renaming '/home/pi/GoPiGo3 Firmware.bin' to '/home/pi/GoPiGo3_Firmware.bin'."
-#mv "/home/pi/GoPiGo3 Firmware.bin" "/home/pi/GoPiGo3_Firmware.bin"
-
-echo "Updating the GoPiGo3 Firmware with '/home/pi/Dexter/GoPiGo3/Firmware/GoPiGo3_Firmware.bin'."
-sudo openocd -f interface/raspberrypi2-native.cfg -c "transport select swd; set CHIPNAME at91samc20j18; source [find target/at91samdXX.cfg]; adapter_khz 250; adapter_nsrst_delay 100; adapter_nsrst_assert_width 100" -c "init; targets; reset halt; program /home/pi/Dexter/GoPiGo3/Firmware/GoPiGo3_Firmware.bin verify; reset" -c "shutdown"
