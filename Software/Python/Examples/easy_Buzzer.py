34f548f677a8575a21dccadea82bc1d46e5a05b1
CleoQc <cleoqc1124@gmail.com>
2017-05-05 14:18:33 -0400

    easy light sensor and easy buzzer examples

---
+++ Software/Python/Examples/easy_Buzzer.py
@@ -0,0 +1,16 @@
+# import the time library for the sleep function
+import time
+
+# import the GoPiGo3 drivers
+import easygopigo3 as easy
+
+# Create an instance of the GoPiGo3 class.
+# GPG will be the GoPiGo3 object.
+GPG = easy.EasyGoPiGo3()
+
+# Create an instance of the Light sensor
+my_buzzer = easy.Buzzer("AD1", GPG)
+
+my_buzzer.sound_on()
+time.sleep(1)
+my_buzzer.sound_off()
