3d6bb557fba88624126565e66ac42d45ed30d4b9
Shoban94 <shobannarayan@gmail.com>
2017-05-11 10:39:48 +0530

    Restricting to look only within Firmware folder
    Not to look inside Archive folder

--- Firmware/gopigo3_flash_firmware.sh
+++ Firmware/gopigo3_flash_firmware.sh
@@ -3,6 +3,6 @@
 # Reads the path of the GoPiGo3 repository
 REPO_PATH=$(readlink -f $(dirname $0) | grep -E -o "^(.*?\\GoPiGo3)")
 # Gets the absolute path of the latest Firmware update 
-FW_UPDATE_FILE=$(sudo find "$REPO_PATH"/Firmware/ -name *.bin)
+FW_UPDATE_FILE=$(sudo find "$REPO_PATH"/Firmware/ -maxdepth 1 -name *.bin)
 echo "Updating the GoPiGo3 Firmware with '$FW_UPDATE_FILE'."
 sudo openocd -f interface/raspberrypi2-native.cfg -c "transport select swd; set CHIPNAME at91samc20j18; source [find target/at91samdXX.cfg]; adapter_khz 250; adapter_nsrst_delay 100; adapter_nsrst_assert_width 100" -c "init; targets; reset halt; program $FW_UPDATE_FILE verify; reset" -c "shutdown"
