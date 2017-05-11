1908930b5590b36ae9ae37082f7a39e7a159c2de
Shoban94 <shobannarayan@gmail.com>
2017-05-11 23:22:52 +0530

    DHT support in GoPiGo3

--- Software/Python/gopigo3.py
+++ Software/Python/gopigo3.py
@@ -752,3 +752,21 @@ class GoPiGo3(object):
         
         # Turn off the LEDs
         self.set_led(self.LED_EYE_LEFT + self.LED_EYE_RIGHT + self.LED_BLINKER_LEFT + self.LED_BLINKER_RIGHT, 0, 0, 0)
+	
+	#   Read the DHT sensor connected to the serial port 
+    def dht(self,sensor_type=0):
+        try:
+            import Adafruit_DHT
+            if sensor_type==0: #blue sensor
+                sensor = Adafruit_DHT.DHT11
+            elif sensor_type==1: #white sensor
+                sensor = Adafruit_DHT.DHT22     
+            pin = 15 #connected to the serial port on the GoPiGo, RX pin
+            humidity, temperature = Adafruit_DHT.read_retry(sensor, pin,retries=3,delay_seconds=.1)
+            if humidity is not None and temperature is not None:
+                return [temperature,humidity]
+            else:
+                return [-2.0,-2.0]
+        except RuntimeError:
+            return [-3.0,-3.0]
+ 
