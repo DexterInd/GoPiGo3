061e9c6accaac228908db4a33e26588925ccf295
Matthew Richardson <matt@dexterindustries.com>
2017-06-27 12:37:31 -0400

    Update (#45)
    * Add GoPiGo3 v3.2.0 schematics
    
    * Improved I2C performance and reliability
    
    * Improve Grove I2C error handling

--- Software/Python/Examples/Grove_I2C.py
+++ Software/Python/Examples/Grove_I2C.py
@@ -23,13 +23,17 @@ I2C_Slave_Address = 0x24 # the address of the I2C slave
 
 try:
     GPG.set_grove_type(GPG.GROVE_1, GPG.GROVE_TYPE.I2C)
-    
+    i = 0
     while(True):
-        GPG.grove_i2c_transfer(GPG.GROVE_1, I2C_Slave_Address, [1])                    # write one byte
-        print(GPG.grove_i2c_transfer(GPG.GROVE_1, I2C_Slave_Address, [0, 1, 0, 1], 1)) # write four bytes and read one byte
-        print(GPG.grove_i2c_transfer(GPG.GROVE_1, I2C_Slave_Address, [], 3))           # read three bytes
+        GPG.grove_i2c_transfer(GPG.GROVE_1, I2C_Slave_Address, [i])                     # write one byte
+        #print(GPG.grove_i2c_transfer(GPG.GROVE_1, I2C_Slave_Address, [0, 1, 0, 1], 1)) # write four bytes and read one byte
+        print(i, GPG.grove_i2c_transfer(GPG.GROVE_1, I2C_Slave_Address, [], 16))        # read sixteen bytes
         
-        time.sleep(0.25)
+        time.sleep(0.1)
+        
+        i += 1
+        if i > 15:
+            i = 0
 
 except KeyboardInterrupt: # except the program gets interrupted by Ctrl+C on the keyboard.
     GPG.reset_all()        # Unconfigure the sensors, disable the motors, and restore the LED to the control of the GoPiGo3 firmware.
