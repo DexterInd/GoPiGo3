8ec3ce83f9bab32280c8a70cd3cb19d9b21a4c31
Robert Lucian CHIRIAC <robert.lucian.chiriac@gmail.com>
2017-06-30 23:53:17 +0300

    ref #3 : modified the [setup.py] a little bit

--- Software/Python/setup.py
+++ Software/Python/setup.py
@@ -8,11 +8,22 @@
 # For more information see https://github.com/DexterInd/GoPiGo3/blob/master/LICENSE.md
 
 import setuptools
+
 setuptools.setup(
-    name="gopigo3",
-    description="Drivers and examples for using the GoPiGo3 in Python",
-    author="Dexter Industries",
-    url="http://www.dexterindustries.com/GoPiGo/",
-    py_modules=['gopigo3','easygopigo3','I2C_mutex'],
-    #install_requires=open('requirements.txt').readlines(),
+    name = "gopigo3",
+    version = "0.0.1",
+    description = "Drivers and examples for using the GoPiGo3 in Python",
+    long_description = " \
+    Within this python package there 2 main modules we can develop with:\n \
+    1. gopigo3 - low-level module for controlling the GPG3\n \
+    2. easygopigo3 - higher-level module built on top of gopigo3 module which\
+    is more user-friendly - allows the user to easily control a GPG3.\n \
+    Updated on 29th of June 2017.\
+    ",
+    author = "Dexter Industries",
+    author_email = "contact@dexterindustries.com",
+    license = 'MIT',
+    url = "http://www.dexterindustries.com/GoPiGo/",
+    py_modules = ['gopigo3','easygopigo3','I2C_mutex'],
+    install_requires = ['spidev', 'fcntl']
 )
