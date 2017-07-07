b3841da8daae8e29c70c7d5e01085ac9e784d5ee
Robert Lucian CHIRIAC <robert.lucian.chiriac@gmail.com>
2017-07-07 14:40:10 +0300

    use assign term instead of create for methods

--- Software/Python/easygopigo3.py
+++ Software/Python/easygopigo3.py
@@ -17,7 +17,7 @@ except Exception as e:
     hardware_connected = False
     print("Unknown issue while importing gopigo3")
     print(e)
-    
+
 
 # from datetime import datetime
 
@@ -300,35 +300,35 @@ class EasyGoPiGo3(gopigo3.GoPiGo3):
                     StartPositionRight - WheelTurnDegrees) is False:
                 time.sleep(0.1)
 
-    def create_light_sensor(self, port):
+    def assign_light_sensor(self, port):
         return LightSensor(port, self)
 
-    def create_sound_sensor(self, port):
+    def assign_sound_sensor(self, port):
         return SoundSensor(port, self)
 
-    def create_ultrasonic_sensor(self, port):
+    def assign_ultrasonic_sensor(self, port):
         return UltraSonicSensor(port, self)
 
-    def create_buzzer(self, port):
+    def assign_buzzer(self, port):
         return Buzzer(port, self)
 
-    def create_led(self, port):
+    def assign_led(self, port):
         return Led(port, self)
 
-    def create_button_sensor(self, port):
+    def assign_button_sensor(self, port):
         return ButtonSensor(port, self)
 
-    def create_linefollower(self, port):
+    def assign_line_follower(self, port):
         return LineFollower(port, self)
 
-    def create_servo(self, port):
+    def assign_servo(self, port):
         return Servo(port, self)
 
-    def create_dht_sensor(self, port = "SERIAL", sensor_type = 0):
+    def assign_dht_sensor(self, port = "SERIAL", sensor_type = 0):
         return DHTSensor(port, port, self, sensor_type)
 
 # the following functions may be redundant
-                
+
 
 #############################################################
 # SENSORS
