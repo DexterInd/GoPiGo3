8d40981b6262cdc73be222685b1c6f71d467e498
Robert Lucian CHIRIAC <robert.lucian.chiriac@gmail.com>
2017-07-18 13:47:12 +0300

    ref #7 : added part of basic robot control project

---
+++ Projects/BasicRobotControl/basic_robot.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+
+"""
+GoPiGo3 for the Raspberry Pi: an open source robotics platform for the Raspberry Pi.
+Copyright (C) 2017  Dexter Industries
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/gpl-3.0.txt>.
+"""
+
+from __future__ import print_function
+from __future__ import division
+from builtins import input
+
+import easygopigo3 as easy
+
+def printLogo():
+    print("   _____       _____ _  _____         ____  ")
+    print("  / ____|     |  __ (_)/ ____|       |___ \ ")
+    print(" | |  __  ___ | |__) || |  __  ___     __) |")
+    print(" | | |_ |/ _ \|  ___/ | | |_ |/ _ \   |__ < ")
+    print(" | |__| | (_) | |   | | |__| | (_) |  ___) |")
+    print("  \_____|\___/|_|   |_|\_____|\___/  |____/ ")
+    print("                                            ")
+
+def printMenu():
+    pass
+
+def Main():
+
+
+if __name__ == "__main__":
+    Main()
