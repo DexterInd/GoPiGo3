77d085047b773f902ea30dc93d45aaf191272273
Robert Lucian CHIRIAC <robert.lucian.chiriac@gmail.com>
2017-08-07 16:11:44 +0300

    ref #7 : feature - added comments & removed debugs in mouse project

--- Projects/MouseControl/mouse_control_movement.py
+++ Projects/MouseControl/mouse_control_movement.py
@@ -23,13 +23,22 @@ from easygopigo3 import *
 from builtins import input
 import threading
 import gopigo3
+import atexit
 
-debug = True # Print raw values when debugging
-signal_not_called = True
+debug = False # Print raw values when debugging
+signal_not_called = True # used to stop reading values from the mouse
 
-MOUSE_THRESH = 20 # How much you can move the mouse from its center before the robot starts moving
+MOUSE_THRESH = 20 # the mouse's sensitivity - the bigger the number, the less sensible the mouse. Idem vice-versa.
 
+# ensure that upon exit the robot stops moving
+def cleanup_func(gopigo3):
+    print("stopping GoPiGo3")
+    gopigo3.stop()
+
+# upon exit, stop reading values from the mouse
+# used in conjunction with signal_not_called var
 def signal_handler(signal, frame):
+    print("stop reading mouse values")
     global signal_not_called
     signal_not_called = False
 
@@ -48,7 +57,7 @@ def getMouseValues(file_input):
     x_axis, y_axis = struct.unpack("bb", buf[1:])
 
     if debug is True:
-        print("Left but: {}, Middle but: {}, Right but: {}, x pos: {}, y pos: {}\n".format(left_button, middle_button, right_button, x_axis, y_axis))
+        print("Left but: {}, Middle but: {}, Right but: {}, x pos: {}, y pos: {}".format(left_button, middle_button, right_button, x_axis, y_axis))
 
     return (left_button, middle_button, right_button, x_axis, y_axis)
 
@@ -66,6 +75,8 @@ def Main():
     print("For moving the robot around using the mouse buttons press 1 and enter.")
     print("For moving the robot around using the movements of the mouse press 2 and enter.")
 
+    # read data from the keyboard
+    # if it fails reading the right values, then the script exits
     try:
         choice = int(input("choice (1/2) = "))
     except ValueError:
@@ -76,7 +87,9 @@ def Main():
         print("Invalid number entered")
         sys.exit(1)
 
-    print("\nWith this script you can control your GoPiGo3 robot with nothing but a mouse.")
+    # now the choice var can either be 1 or 2
+    # show different menus depending on the choice var
+    print("\nWith this script you can control your GoPiGo3 robot with a wireless mouse.")
     if choice == 1:
         print("1. Left + Right buttons of the mouse - move the GoPiGo3 forward")
         print("2. Left button of the mouse - move the GoPiGo3 to the left")
@@ -89,8 +102,10 @@ def Main():
         print("4. Move the mouse to the right - for rotating the GoPiGo3 to the right")
 
     # Wait for an input to start
-    input("Press Enter to start")
+    input("\nPress Enter to start")
 
+    # create an instance of the EasyGoPiGo3 class
+    # if it fails instantiating the object, then the scripts exits
     try:
         robot = EasyGoPiGo3()
 
@@ -106,9 +121,13 @@ def Main():
         print("Something went wrong")
         sys.exit(1)
 
-    print("\nIn order to stop the script, press CTRL-C and move your mouse a little bit")
-    print('This script has blocking-methods and moving the mouse will get the script "moving" ')
+    # stops the robot from moving when exiting the script
+    # the cleanup_func is called after the signal_handler function
+    atexit.register(cleanup_func, gopigo3 = robot)
 
+    print("\nIn order to stop the script, press CTRL-C and move your mouse a little bit")
+
+    # open file for reading the continuous stream of data from the mouse
     with open("/dev/input/mice", "rb") as mouse_input:
 
         left_button = 0
@@ -117,38 +136,64 @@ def Main():
         x_axis = 0
         y_axis = 0
 
+        # as long as CTRL-C hasn't been pressed
         while signal_not_called:
 
+            # read the mouse's values
+            # this is a blocking function
             (left_button, middle_button, right_button, x_axis, y_axis) = getMouseValues(mouse_input)
 
+            # if we have the first choice (see the menu)
             if choice == 1:
 
+                # when both the mouse's buttons are pressed
+                # move forward
                 if left_button == True and right_button == True:
                     robot.forward()
+                # when just the left button is pressed
+                # move to the left
                 elif left_button == True and right_button == False:
                     robot.left()
+                # when just the right button is pressed
+                # move to the right
                 elif left_button == False and right_button == True:
                     robot.right()
+                # when the middle button is pressed
+                # move backward
                 elif middle_button == True:
                     robot.backward()
+                # when no button is pressed
+                # stop the robot from moving
                 elif middle_button == False or (left_button == False and right_button == False):
                     robot.stop()
 
+            # if we have the second choice (see the menu)
             else:
 
+                # if the mouse is moved to the left
+                # then move the robot to the left
                 if x_axis < -MOUSE_THRESH:
                     robot.left()
+                # if the mouse is moved to the right
+                # then move the robot to the right
                 elif x_axis > MOUSE_THRESH:
                     robot.right()
+                # if the mouse is moved backward
+                # then move the robot backward
                 elif y_axis < -MOUSE_THRESH:
                     robot.backward()
+                # if the mouse is moved forward
+                # then move the robot forward
                 elif y_axis > MOUSE_THRESH:
                     robot.forward()
+                # if the mouse is not moving in any direction
+                # then stop the robot from moving
                 else:
                     robot.stop()
 
             sleep(0.10)
 
 if __name__ == "__main__":
+    # when CTRL-C is pressed, this is will ensure signal_handler is called
     signal.signal(signal.SIGINT, signal_handler)
     Main()
