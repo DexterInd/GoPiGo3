a1064d0ca3ee1a24d3e2885da202bfc09475e985
Matthew Richardson <matt@dexterindustries.com>
2017-08-21 10:39:10 -0400

    Update gopigo3_power.py comments

--- Software/gopigo3_power.py
+++ Software/gopigo3_power.py
@@ -6,8 +6,13 @@
 # Released under the MIT license (http://choosealicense.com/licenses/mit/).
 # For more information, see https://github.com/DexterInd/GoPiGo3/blob/master/LICENSE.md
 #
-# This code is for power management on a Raspberry Pi
-# GPIO 23 needs to remain low impedance (output) set to a HIGH state. If GPIO 23 gets left floating (high impedance) the GoPiGo3 assumes the RPi has shut down fully. SW should never write GPIO 23 to LOW or set it as an INPUT.
+# This code is for power management on a Raspberry Pi with GoPiGo3.
+#
+# GPIO 22 will be configured as input with pulldown. If pulled high, the RPi will halt.
+#
+# GPIO 23 needs to remain low impedance (output) set to a HIGH state. If GPIO 23 gets
+# left floating (high impedance) the GoPiGo3 assumes the RPi has shut down fully.
+# SW should never write GPIO 23 to LOW or set it as an INPUT.
 
 
 import RPi.GPIO as GPIO
