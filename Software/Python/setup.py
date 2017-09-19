1261a667f976b3bf0844f13285042bc057d7de26
Robert Lucian Chiriac <robert.lucian.chiriac@gmail.com>
2017-09-19 23:30:53 +0300

    incremented version

--- Software/Python/setup.py
+++ Software/Python/setup.py
@@ -18,7 +18,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = "gopigo3",
-    version = "1.1.0",
+    version = "1.1.1",
 
     description = "Drivers and Examples for using the GoPiGo3 in Python",
     long_description = description,
