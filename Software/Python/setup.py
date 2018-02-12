e4c4e8479b24ae41ed38747e9ffad1a694bf3ff9
CleoQc <cleoqc1124@gmail.com>
2018-02-12 17:24:17 -0500

    removing i2c_mutex which will be found in script_tools instead

--- Software/Python/setup.py
+++ Software/Python/setup.py
@@ -43,6 +43,6 @@ setup(
     keywords = ['robot', 'gopigo', 'gopigo3', 'dexter industries', 'learning', 'education'],
 
     packages=find_packages(),
-    py_modules = ['gopigo3','easygopigo3','I2C_mutex'],
+    py_modules = ['gopigo3','easygopigo3'],
     install_requires = ['spidev']
 )
