625d81788af76d5a7171306144e4ed6434065a8b
Christian Rauch <Rauch.Christian@gmx.de>
2018-02-27 21:25:38 +0000

    CMake build configuration for C++ library (#150)
    * rename C++ examples files to *.cpp
    
    * remove duplicated function 'fatal_error'
    
    * all examples: use 'GoPiGo3.h' header and compile 'GoPiGo3.cpp' into examples
    
    * throw exceptions instead of terminating program
    
    * remove whitespaces
    
    * CMake file for compiling GoPiGo3 library and examples
    
    * install library and header files and make them available to other projects
    
    * ROS package definition for GoPiGo3 library

--- Software/C/Examples/info.cpp
+++ Software/C/Examples/info.cpp
@@ -11,40 +11,40 @@
  *  Results: Print information about the attached GoPiGo3.
  *
  *  Example compile command:
- *    g++ -o program "info.c"
+ *    g++ -o info info.cpp ../GoPiGo3.cpp -I..
  *  Example run command:
- *    sudo ./program
+ *    sudo ./info
  *
  */
 
-#include "GoPiGo3.cpp" // for GoPiGo3
+#include <GoPiGo3.h>   // for GoPiGo3
 #include <stdio.h>     // for printf
 
 GoPiGo3 GPG; // Create a GoPiGo3 instance
 
 int main(){
   char str[33]; // Room for the 32-character serial number string plus the NULL terminator.
-  
+
   // Make sure that the GoPiGo3 is communicating and that the firmware is compatible with the drivers.
   // pass 'false' to detect() to make the error non-critical (return the error instead of exiting the program).
   if(GPG.detect(false) == ERROR_NONE){
     printf("\nGoPiGo3 info:\n");
-    
+
     GPG.get_manufacturer(str);
     printf("  Manufacturer    : %s\n", str);
-    
+
     GPG.get_board(str);
     printf("  Board           : %s\n", str);
-    
+
     GPG.get_id(str);
     printf("  Serial Number   : %s\n", str);
-    
+
     GPG.get_version_hardware(str);
     printf("  Hardware version: %s\n", str);
-    
+
     GPG.get_version_firmware(str);
     printf("  Firmware version: %s\n", str);
-    
+
     printf("  Battery voltage : %.3f\n", GPG.get_voltage_battery());
     printf("  5v voltage      : %.3f\n", GPG.get_voltage_5v());
   }else{
