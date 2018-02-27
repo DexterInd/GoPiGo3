e98418a180154d4ad2fd2f5c9e1bd0496e2a2413
CleoQc <cleoqc1124@gmail.com>
2018-02-27 16:08:12 -0500

    adjust documentation for turn_degrees blocking parameter

--- Software/Python/easygopigo3.py
+++ Software/Python/easygopigo3.py
@@ -516,7 +516,7 @@ class EasyGoPiGo3(gopigo3.GoPiGo3):
         | Makes the `GoPiGo3`_ robot turn at a specific angle while staying in the same spot.
 
         :param float degrees: The angle in degress at which the `GoPiGo3`_ has to turn. For rotating the robot to the left, ``degrees`` has to negative, and make it turn to the right, ``degrees`` has to be positive.
-        :param boolean blocking = False: Set it as a blocking or non-blocking method.
+        :param boolean blocking = True: Set it as a blocking or non-blocking method.
 
         ``blocking`` parameter can take the following values:
 
