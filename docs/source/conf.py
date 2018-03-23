958d4d932a805b39c96a4ab1e714f41a5c81c0e3
Robert Lucian Chiriac <robert.lucian.chiriac@gmail.com>
2018-03-23 19:09:02 +0200

    hotfix - mock I2C_mutex file (#166)
    * this doesn't have to be merged into the dev branch

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -30,7 +30,7 @@ class Mock(MagicMock):
     def __getattr__(cls, name):
             return MagicMock()
 
-MOCK_MODULES = ['spidev', 'fcntl']
+MOCK_MODULES = ['spidev', 'fcntl', 'I2C_mutex']
 sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
 # -- General configuration ------------------------------------------------
