16a09ecc2a65ad5e22014a093ebec0d8cfad6bce
CleoQc <cleoqc1124@gmail.com>
2018-04-20 10:41:02 -0400

    remove old libraries

--- Install/install.sh
+++ Install/install.sh
@@ -93,6 +93,10 @@ else
 fi
 
 echo ""
+
+# remove old libraries, otherwise Mutex is taken from there instead
+sudo rm /usr/local/lib/python2.7/dist-packages/gopigo3*.egg
+sudo rm /usr/local/lib/python3.4/dist-packages/gopigo3*.egg
 cd $REPO_PATH/Software/Python/
 python setup.py install --force
 python3 setup.py install --force
