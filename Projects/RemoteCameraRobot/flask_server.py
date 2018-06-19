83f518e376b2b50b307a518e9236894e36e972d4
Robert Lucian CHIRIAC <robert.lucian.chiriac@gmail.com>
2018-06-19 22:07:44 +0300

    feature - replace UV4L with picamera

--- Projects/RemoteCameraRobot/flask_server.py
+++ Projects/RemoteCameraRobot/flask_server.py
@@ -1,30 +1,84 @@
 # Dexter Industries GoPiGo3 Remote Camera robot
 # With this project you can control your Raspberry Pi Robot, the GoPiGo3, with a phone, tablet, or browser.
 # Remotely view your robot as first person in your browser.
-# You MUST:
-# - Run this in python3
-# - Run this with sudo powers.
 #
-# To Run:  sudo python3 flask_server.py
+# You MUST run this with python3
+# To Run:  python3 flask_server.py
+
+import signal
+import sys
+import logging
+from time import sleep
+
+# check if it's ran with Python3
+assert sys.version_info[0:1] == (3,)
+
+# imports needed for web server
+from flask import Flask, jsonify, render_template, request, Response, send_from_directory, url_for
+from werkzeug.serving import make_server
+from gopigo3 import FirmwareVersionError
+from easygopigo3 import EasyGoPiGo3
+
+# imports needed for stream server
+import io
+import picamera
+import socketserver
+from threading import Condition, Thread, Event
+from http import server
+
+logging.basicConfig(level = logging.DEBUG)
+
+# for triggering the shutdown procedure when a signal is detected
+keyboard_trigger = Event()
+def signal_handler(signal, frame):
+    logging.info('Signal detected. Stopping threads.')
+    keyboard_trigger.set()
+
+#######################
+### Web Server Stuff ##
+#######################
 
 # Directory Path can change depending on where you install this file.  Non-standard installations
 # may require you to change this directory.
 directory_path = '/home/pi/Dexter/GoPiGo3/Projects/RemoteCameraRobot/static'
 
-try:
-    from flask import Flask, jsonify, render_template, request, Response, send_from_directory, url_for
-    from easygopigo3 import EasyGoPiGo3
-except Exception as e:
-    print("Error: " + str(e))
-    print("Error occured!  Make sure that your ran this program using python3!")
-    print("Try again with the command sudo python3 flask_server.py")
-
 MAX_FORCE = 5.0
 MIN_SPEED = 100
 MAX_SPEED = 300
-gopigo3_robot = EasyGoPiGo3()
+try:
+    gopigo3_robot = EasyGoPiGo3()
+except IOError:
+    logging.critical('GoPiGo3 is not detected.')
+    sys.exit(1)
+except FirmwareVersionError:
+    logging.critical('GoPiGo3 firmware needs to be updated')
+    sys.exit(2)
+except Exception:
+    logging.critical("Unexpected error when initializing GoPiGo3 object")
+    sys.exit(3)
 
+HOST = "0.0.0.0"
+WEB_PORT = 5000
 app = Flask(__name__, static_url_path='')
+
+class WebServerThread(Thread):
+    '''
+    Class to make the launch of the flask server non-blocking.
+    Also adds shutdown functionality to it.
+    '''
+    def __init__(self, app, host, port):
+        Thread.__init__(self)
+        self.srv = make_server(host, port, app)
+        self.ctx = app.app_context()
+        self.ctx.push()
+
+    def run(self):
+        logging.info('Starting Flask server')
+        self.srv.serve_forever()
+
+    def shutdown(self):
+        logging.info('Stopping Flask server')
+        self.srv.shutdown()
 
 @app.route("/robot", methods = ["POST"])
 def robot_commands():
@@ -70,7 +124,7 @@ def robot_commands():
     elif state == 'stop':
         gopigo3_robot.stop()
     else:
-        app.logger.warning('unknown state sent')
+        app.logging.warning('unknown state sent')
 
     resp = Response()
     resp.mimetype = "application/json"
@@ -91,7 +145,108 @@ def page(page_name):
 def send_static(path):
     return send_from_directory(directory_path, path)
 
+#############################
+### Video Streaming Stuff ###
+#############################
+
+class StreamingOutput(object):
+    '''
+    Class to which the video output is written to.
+    The buffer of this class is then read by StreamingHandler continuously.
+    '''
+    def __init__(self):
+        self.frame = None
+        self.buffer = io.BytesIO()
+        self.condition = Condition()
+
+    def write(self, buf):
+        if buf.startswith(b'\xff\xd8'):
+            # New frame, copy the existing buffer's content and notify all
+            # clients it's available
+            self.buffer.truncate()
+            with self.condition:
+                self.frame = self.buffer.getvalue()
+                self.condition.notify_all()
+            self.buffer.seek(0)
+        return self.buffer.write(buf)
+
+class StreamingHandler(server.BaseHTTPRequestHandler):
+    '''
+    Implementing GET request for the video stream.
+    '''
+    def do_GET(self):
+        if self.path == '/stream.mjpg':
+            self.send_response(200)
+            self.send_header('Age', 0)
+            self.send_header('Cache-Control', 'no-cache, private')
+            self.send_header('Pragma', 'no-cache')
+            self.send_header('Content-Type', 'multipart/x-mixed-replace; boundary=FRAME')
+            self.end_headers()
+            try:
+                while True:
+                    with output.condition:
+                        output.condition.wait()
+                        frame = output.frame
+                    self.wfile.write(b'--FRAME\r\n')
+                    self.send_header('Content-Type', 'image/jpeg')
+                    self.send_header('Content-Length', len(frame))
+                    self.end_headers()
+                    self.wfile.write(frame)
+                    self.wfile.write(b'\r\n')
+            except Exception as e:
+                logging.warning(
+                    'Removed streaming client %s: %s',
+                    self.client_address, str(e))
+        else:
+            self.send_error(404)
+            self.end_headers()
+
+class StreamingServer(socketserver.ThreadingMixIn, server.HTTPServer):
+    allow_reuse_address = True
+    daemon_threads = True
+
+#############################
+### Aggregating all calls ###
+#############################
+
 if __name__ == "__main__":
-    print("Running server!")
-    print("Connect on either 'http://dex.local:5000' in your browser.")
-    app.run(debug = True, host = "0.0.0.0")
+    # registering both types of signals
+    signal.signal(signal.SIGINT, signal_handler)
+    signal.signal(signal.SIGTERM, signal_handler)
+
+    # firing up the video camera (pi camera)
+    camera = picamera.PiCamera(resolution='320x240', framerate=30)
+    output = StreamingOutput()
+    camera.start_recording(output, format='mjpeg')
+    logging.info("Started recording with picamera")
+    STREAM_PORT = 5001
+    stream = StreamingServer((HOST, STREAM_PORT), StreamingHandler)
+
+    # starting the video streaming server
+    streamserver = Thread(target = stream.serve_forever)
+    streamserver.start()
+    logging.info("Started stream server for picamera")
+
+    # starting the web server
+    webserver = WebServerThread(app, HOST, WEB_PORT)
+    webserver.start()
+    logging.info("Started Flask web server")
+
+    # and run it indefinitely
+    while not keyboard_trigger.is_set():
+        sleep(0.5)
+
+    # until some keyboard event is detected
+    logging.info("Keyboard event detected")
+
+    # trigger shutdown procedure
+    webserver.shutdown()
+    camera.stop_recording()
+    stream.shutdown()
+
+    # and finalize shutting them down
+    webserver.join()
+    streamserver.join()
+    logging.info("Stopped all threads")
+
+    sys.exit(0)
