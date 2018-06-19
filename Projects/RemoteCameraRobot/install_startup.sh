699dc5b2fa6fc2400e37cabb58a446670c23867f
Robert Lucian CHIRIAC <robert.lucian.chiriac@gmail.com>
2018-06-19 22:11:12 +0300

    feature - remove UV4L and rename main python script

--- Projects/RemoteCameraRobot/install_startup.sh
+++
@@ -1,7 +0,0 @@
-# Installation file to make the Camera Robot start on boot.
-# This will add the camerarobot start on boot to SystemD on Stretch
-
-sudo cp camerarobot.service /etc/systemd/system/
-sudo chmod 644 /etc/systemd/system/camerarobot.service
-sudo systemctl daemon-reload
-sudo systemctl enable camerarobot.service
