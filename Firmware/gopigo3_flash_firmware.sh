77de93f0335f847f434db962618e9206c21a86a7
CleoQc <cleoqc1124@gmail.com>
2018-10-11 14:36:41 -0400

    slow down firmware update

--- Firmware/gopigo3_flash_firmware.sh
+++ Firmware/gopigo3_flash_firmware.sh
@@ -5,4 +5,4 @@ REPO_PATH=$(readlink -f $(dirname $0) | grep -E -o "^(.*?\\GoPiGo3)")
 # Gets the absolute path of the latest Firmware update 
 FW_UPDATE_FILE=$(sudo find "$REPO_PATH"/Firmware/ -maxdepth 1 -name *.bin)
 echo "Updating the GoPiGo3 Firmware with '$FW_UPDATE_FILE'."
-sudo openocd -f interface/raspberrypi2-native.cfg -c "transport select swd; set CHIPNAME at91samc20j18; source [find target/at91samdXX.cfg]; adapter_khz 250; adapter_nsrst_delay 100; adapter_nsrst_assert_width 100" -c "init; targets; reset halt; program $FW_UPDATE_FILE verify; reset" -c "shutdown"
+sudo openocd -f interface/raspberrypi2-native.cfg -c "transport select swd; set CHIPNAME at91samc20j18; source [find target/at91samdXX.cfg]; adapter_khz 50; adapter_nsrst_delay 100; adapter_nsrst_assert_width 100" -c "init; targets; reset halt; program $FW_UPDATE_FILE verify; reset" -c "shutdown"
