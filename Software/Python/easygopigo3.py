e52cd123cf89591fec67bc5ff7c61c1b955c36cf
Nicole Parrot <cleo@parrot.ca>
2021-10-21 15:14:00 -0400

    up version number
    Keep track of version number. 
    1.3.1 has the steer fix

--- Software/Python/easygopigo3.py
+++ Software/Python/easygopigo3.py
@@ -13,7 +13,7 @@ import json
 import easysensors
 from I2C_mutex import Mutex
 
-__version__ = "1.3.0"
+__version__ = "1.3.1"
 
 try:
     from di_sensors import easy_line_follower, easy_distance_sensor, easy_light_color_sensor, easy_inertial_measurement_unit
