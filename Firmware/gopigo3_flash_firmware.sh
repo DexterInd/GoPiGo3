f46e85733a4a5125a41e7dd60cec241905e28201
Nicole Parrot <cleo@parrot.ca>
2022-02-18 09:44:05 -0500

    Allow firmware flashing on a Pi4

--- Firmware/gopigo3_flash_firmware.sh
+++ Firmware/gopigo3_flash_firmware.sh
@@ -5,6 +5,7 @@
 /usr/bin/python3 /home/pi/Dexter/GoPiGo3/Firmware/check_if_firmware_update_is_needed.py
 if [ $? -eq 1 ]
 then
+  echo "Firmware is up to date and working properly."
   echo "Exiting firmware upgrade script file without changing anything."
   exit
 else
@@ -23,9 +24,8 @@ elif [ "$RPI_VERSION" == "RPI2" ] || [ "$RPI_VERSION" == "RPI3" ] || [ "$RPI_VER
     INTERFACE_FILE="rpi2.cfg"
 elif [ "$RPI_VERSION" == "RPI4" ]; then
     # use rpi4 interface config file
-    echo "Flashing the firmware using a Pi4 is not currently supported"
-    exit
-    INTERFACE_FILE="rpi4.cfg"
+    echo "Flashing the firmware using a Pi4"
+    INTERFACE_FILE="rpi4-sysfsgpio.cfg"
 fi
 
 if [ "$INTERFACE_FILE" == "none" ]; then
@@ -34,18 +34,25 @@ if [ "$INTERFACE_FILE" == "none" ]; then
 else
     echo "Using interface file '$INTERFACE_FILE' for RPi version '$RPI_VERSION'."
 
-    # Read the path of the GoPiGo3 repository
-    REPO_PATH=$(readlink -f $(dirname $0) | grep -E -o "^(.*?\\GoPiGo3)")
-
+    SCRIPT_DIR=$(cd $(dirname "${BASH_SOURCE[0]}") && pwd)
+    echo $SCRIPT_DIR
     # Get the absolute path of the latest Firmware update
-    FIRMWARE_FILE=$(sudo find "$REPO_PATH"/Firmware/ -maxdepth 1 -name *.bin)
+    FIRMWARE_FILE=$(sudo find $SCRIPT_DIR -maxdepth 1 -name "*.bin")
+
+    echo $FIRMWARE_FILE
 
     if [ "$FIRMWARE_FILE" == "" ]; then
         echo "Failed to find firmware file."
     else
         echo "Updating the GoPiGo3 Firmware with '$FIRMWARE_FILE'"
 
-        # flash the firmware
-        sudo openocd -f interface/$INTERFACE_FILE -c "transport select swd; set CHIPNAME at91samc20j18; source [find target/at91samdXX.cfg]; adapter_khz 50; adapter_nsrst_delay 100; adapter_nsrst_assert_width 100" -c "init; targets; reset halt; program $FIRMWARE_FILE verify; reset" -c "shutdown"
+        if [ "$INTERFACE_FILE" == "rpi4-sysfsgpio.cfg" ]; then
+            sudo openocd -f $SCRIPT_DIR/$INTERFACE_FILE -c "transport select swd; set CHIPNAME at91samc20j18; source [find target/at91samdXX.cfg]; adapter srst delay 100; adapter srst pulse_width 100" -c "init; targets; reset halt; program $FIRMWARE_FILE verify; reset" -c "shutdown"
+        else
+            # flash the firmware - P2 and Pi3
+            sudo openocd -f $SCRIPT_DIR/$INTERFACE_FILE -c "transport select swd; set CHIPNAME at91samc20j18; source [find target/at91samdXX.cfg]; adapter_khz 50; adapter_nsrst_delay 100; adapter_nsrst_assert_width 100" -c "init; targets; reset halt; program $FIRMWARE_FILE verify; reset" -c "shutdown"
+        fi
+
+        echo $(python -c "import gopigo3;g=gopigo3.GoPiGo3();print('Success.')")
     fi
 fi
