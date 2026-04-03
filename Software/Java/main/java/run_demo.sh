ba3d834b1ca186f42c69231730974f74294479fd
Nicole Parrot <cleo@parrot.ca>
2026-04-03 15:30:43 -0400

    Remove CSharp, Go, Java, NodeJS and Scratch

--- Software/Java/main/java/run_demo.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-sudo javac -classpath .:classes:/opt/pi4j/lib/'*':/opt/java-json/'*' -d . com/dexterindustries/DemoEasyGoPiGo3.java
-sudo java -classpath .:classes:/opt/pi4j/lib/'*':/opt/java-json/'*' com/dexterindustries/DemoEasyGoPiGo3
-
--- Software/NodeJS/examples/custom/ultrasonic.js
+++
@@ -1,8 +0,0 @@
-const sleep = require('sleep');
-const EasyGopigo3 = require('../../lib/easyGopigo3');
-
-const gpg = new EasyGopigo3();
-const sensor = gpg.initUltrasonicSensor('AD1');
-
-console.log(sensor.read(), sensor.readMm(), sensor.readInches());
-console.log('Is too close', sensor.isTooClose());
